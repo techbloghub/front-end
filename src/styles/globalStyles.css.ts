ced7041e19fe78a8432344415da4d76ca5a6e4f6
eggmun <eggmun98@gmail.com>
2024-12-21 17:06:58 +0900

    feat: 테마함수 사용

--- src/styles/globalStyles.css.ts
+++ src/styles/globalStyles.css.ts
@@ -1,17 +1,9 @@
-import { createGlobalTheme, globalStyle } from '@vanilla-extract/css';
+import { createGlobalTheme, createTheme, createThemeContract, globalStyle } from '@vanilla-extract/css';
 
-export const vars = createGlobalTheme(':root', {
+export const global = createGlobalTheme(':root', {
   color: {
     primary: '#ffff',
   },
-});
-
-globalStyle('html, body', {
-  margin: 0,
-  padding: 0,
-  backgroundColor: '#000',
-  color: 'white',
-  height: '100%',
 });
 
 globalStyle('*', {
@@ -21,4 +13,35 @@ globalStyle('*', {
 globalStyle('a', {
   textDecoration: 'none',
   color: '#fff',
+});
+
+const themeColor = createThemeContract({
+  color: {
+    mainBackground: null,
+    contentBackground: null,
+  },
+});
+
+export const lightTheme = createTheme(themeColor, {
+  color: {
+    mainBackground: '#000000',
+    contentBackground: '#ffffff',
+  },
+});
+
+export const darkTheme = createTheme(themeColor, {
+  color: {
+    mainBackground: '#ffffff',
+    contentBackground: '#2c2c2c',
+  },
+});
+
+export const vars = { ...global, themeColor };
+
+globalStyle('body', {
+  margin: 0,
+  padding: 0,
+  backgroundColor: `hsl(${vars.themeColor.color.mainBackground})`,
+  color: 'white',
+  height: '100%',
 });
