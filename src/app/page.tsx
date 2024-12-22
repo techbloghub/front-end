d9c8f7388eb1a4cc49ef9ec1500e94ce67854ccd
eggmun <eggmun98@gmail.com>
2024-12-22 15:10:25 +0900

    feat: SearchToolbar 위젯 컴포넌트 선언 및 사용

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,13 @@
-import Search from '@/components/features/Inputs/Search/Search';
 import Card from '@/components/features/Items/Card/Card';
 import Header from '@/components/widgets/Header/Header';
+import SearchToolbar from '@/components/widgets/SearchToolbar/SearchToolbar';
 import * as styles from '@/styles/rootStyles.css';
 
 export default function RootPage() {
   return (
     <div className={styles.wrapper}>
       <Header />
-      <Search />
+      <SearchToolbar />
       <Card />
     </div>
   );
