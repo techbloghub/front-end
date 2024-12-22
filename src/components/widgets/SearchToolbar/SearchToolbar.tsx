45f606abbd95a4a4e9c1d572af6b66539ada6140
eggmun <eggmun98@gmail.com>
2024-12-22 15:10:25 +0900

    feat: SearchToolbar 위젯 컴포넌트 선언 및 사용

---
+++ src/components/widgets/SearchToolbar/SearchToolbar.tsx
@@ -0,0 +1,11 @@
+import Slider from '@/components/features/Common/Slider/Slider';
+import Search from '@/components/features/Inputs/Search/Search';
+
+export default function SearchToolbar() {
+  return (
+    <>
+      <Search />
+      <Slider />
+    </>
+  );
+}
