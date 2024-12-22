a077d914a81748f3f632345e866755f6730dd70e
eggmun <eggmun98@gmail.com>
2024-12-22 14:51:28 +0900

    rename: Search 컴포넌트 폴더 경로 변경 - Layouts -> Inputs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import Card from '@/components/Layouts/Card/Card';
 import Header from '@/components/Layouts/Header/Header';
-import Search from '@/components/Layouts/Search/Search';
+import Search from '@/components/Inputs/Search/Search';
 import * as styles from '@/styles/rootStyles.css';
 
 export default function RootPage() {
