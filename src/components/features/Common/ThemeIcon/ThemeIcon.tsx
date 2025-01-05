022b7f12e4dbd001001d445182ac8e08a218796b
eggmun <eggmun98@gmail.com>
2025-01-05 15:52:12 +0900

    refactor: ThemeIconProps, Theme 타입 사용

--- src/components/features/Common/ThemeIcon/ThemeIcon.tsx
+++ src/components/features/Common/ThemeIcon/ThemeIcon.tsx
@@ -1,14 +1,10 @@
 import Moon from '@/components/atoms/Icons/Moon/Moon';
 import Sun from '@/components/atoms/Icons/Sun/Sun';
+import { Theme } from '@/types/theme';
+import { ThemeIconProps } from './ThemeIcon.types';
 
-export default function ThemeIcon({
-  setTheme,
-  resolvedTheme,
-}: {
-  setTheme: (theme: string) => void;
-  resolvedTheme: string | undefined;
-}) {
-  return resolvedTheme === 'dark' ? (
+export default function ThemeIcon({ setTheme, resolvedTheme }: ThemeIconProps) {
+  return resolvedTheme === Theme.DARK ? (
     <Sun onClick={() => setTheme(resolvedTheme === 'dark' ? 'light' : 'dark')} />
   ) : (
     <Moon onClick={() => setTheme(resolvedTheme === 'dark' ? 'light' : 'dark')} />
