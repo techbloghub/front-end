4c95d1e8fe871ada62b5f1e714a781f32ed6fc45
eggmun <eggmun98@gmail.com>
2025-02-16 16:59:28 +0900

    refactor(tag-type): getTags 함수 API 응답 타입 추가

--- src/domains/tag/api/tags.api.ts
+++ src/domains/tag/api/tags.api.ts
@@ -1,3 +1,4 @@
 import { Get } from '@/configs/fetch/http.api';
+import type { TagResponse } from '@/domains/tag/tpyes/tag.type';
 
-export const getTags = async (search?: string) => Get(`/tags`, { search });
+export const getTags = async (search?: string) => Get<TagResponse>(`/tags`, { search });
