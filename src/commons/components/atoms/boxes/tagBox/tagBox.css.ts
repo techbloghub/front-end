338739e6454fba4319c5075592377125726ce1c2
eggmun <eggmun98@gmail.com>
2025-02-16 18:15:44 +0900

    feat: TagBox Atom 컴포넌트 구현

---
+++ src/commons/components/atoms/boxes/tagBox/tagBox.css.ts
@@ -0,0 +1,9 @@
+import { style } from '@vanilla-extract/css';
+
+export const tagBox = style({
+  padding: '8px 10px',
+  borderRadius: '50px',
+  backgroundColor: '#6A6868',
+
+  fontSize: '12px',
+});
