cc2148148cbd0354a02ec0e633ba02d27f30771d
eggmun <eggmun98@gmail.com>
2025-02-16 17:41:00 +0900

    rename: configs 폴더 경로 변경

--- src/domains/tag/api/tags.api.ts
+++ src/domains/tag/api/tags.api.ts
@@ -1,4 +1,4 @@
-import { Get } from '@/configs/fetch/http.api';
+import { Get } from '@/commons/configs/fetch/http.api';
 import type { TagResponse } from '@/domains/tag/tpyes/tag.type';
 
 export const getTags = async (search?: string) => Get<TagResponse>(`/tags`, { search });
