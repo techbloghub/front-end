a1fb6f07850f1ccad9573370a84425059a616853
eggmun <eggmun98@gmail.com>
2025-03-09 17:13:13 +0900

    stlye: 기본 검색창 - placeholder 추가

--- src/commons/components/features/Inputs/CollapsedSearch/CollapsedSearch.css.ts
+++ src/commons/components/features/Inputs/CollapsedSearch/CollapsedSearch.css.ts
@@ -3,6 +3,13 @@ import { style } from '@vanilla-extract/css';
 export const collapsedSearchWrapper = style({
   display: 'flex',
   flexDirection: 'row',
+  alignItems: 'center',
 
   marginRight: 'auto',
+});
+
+export const placeholder = style({
+  color: '#999',
+
+  fontSize: '14px',
 });
--- src/commons/components/features/Inputs/CollapsedSearch/CollapsedSearch.tsx
+++ src/commons/components/features/Inputs/CollapsedSearch/CollapsedSearch.tsx
@@ -2,6 +2,7 @@ import { collapsedSearchWrapper } from './CollapsedSearch.css';
 import { KeyboardEvent } from 'react';
 import Image from 'next/image';
 import { TagList } from '../../Lists/TagList/TagList';
+import * as styles from './CollapsedSearch.css';
 
 export const CollapsedSearch = ({
   tagList,
@@ -13,13 +14,13 @@ export const CollapsedSearch = ({
   return (
     <>
       <div
-        className={collapsedSearchWrapper}
+        className={styles.collapsedSearchWrapper}
         role="button"
         tabIndex={0}
         aria-label="검색 확장 버튼"
         onKeyDown={handleKeyDown}
       >
-        <TagList tags={tagList} />
+        {tagList.length > 0 ? <TagList tags={tagList} /> : <p className={styles.placeholder}>검색어를 입력해주세요.</p>}
       </div>
       <Image src="/images/buttons/readingGlasses.png" alt="ReadingGlasses" width={22} height={30} />
     </>
