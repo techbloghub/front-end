94c94798d97c37466f91e1e922bb195a743b0e4e
eggmun <eggmun98@gmail.com>
2025-05-05 16:33:02 +0900

    feat(posting): React Query 통합을 통해 태그 관리 및 검색 모드 기능 구현

--- src/domains/posting/hooks/queryPostings/useQueryPostings.ts
+++ src/domains/posting/hooks/queryPostings/useQueryPostings.ts
@@ -1,12 +1,33 @@
-import { useQuery } from '@tanstack/react-query';
+import { useInfiniteQuery } from '@tanstack/react-query';
 import { getPostings } from '../../api/posting.api';
 import { QUERY_KEYS } from '@/commons/constants/keys/queryKeys';
+import { PostingResponse } from '../../types/postings.type';
+import { useSearchModeStore } from '@/commons/store/searchMode/useSearchModeStore';
+import { SearchMode } from '@/commons/types/search/search.type';
 
-export const useQueryPostings = (tags?: string, cursor?: string, title?: string) => {
-  const { data, isLoading, error } = useQuery({
-    queryKey: [QUERY_KEYS.POSTINGS, tags, cursor, title],
-    queryFn: () => getPostings(tags, cursor, title),
+export const useQueryPostings = (tags?: string, title?: string) => {
+  const { searchMode } = useSearchModeStore();
+
+  const tagsParam = searchMode === SearchMode.TAG ? tags : undefined;
+  const titleParam = searchMode === SearchMode.TITLE ? title : undefined;
+
+  const { data, isLoading, error, fetchNextPage, hasNextPage, isFetchingNextPage } = useInfiniteQuery({
+    queryKey: [QUERY_KEYS.POSTINGS, tagsParam, titleParam],
+    queryFn: ({ pageParam }) => getPostings(tagsParam, pageParam, titleParam),
+    initialPageParam: undefined,
+    getNextPageParam: (lastPage: PostingResponse) => {
+      return lastPage.has_next_page ? lastPage.postings[lastPage.postings.length - 1].posting_id.toString() : undefined;
+    },
+    enabled: Boolean(tagsParam) || Boolean(titleParam) || (!tagsParam && !titleParam),
   });
 
-  return { data, isLoading, error };
+  return {
+    data,
+    isLoading,
+    error,
+    fetchNextPage,
+    hasNextPage: !!hasNextPage,
+    isFetchingNextPage,
+    postings: data?.pages.flatMap((page) => page.postings) || [],
+  };
 };
