3133409fcae2a8c8b9b0bc0742a497bacf59592f
eggmun <eggmun98@gmail.com>
2025-05-31 17:38:36 +0900

    remove: UI 리뉴얼을 위한 사용하지 않은 UI 컴포넌트 제거

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,3 @@
-import RemoteControl from '@/commons/components/features/Common/RemoteControl/RemoteControl';
-import Card from '@/commons/components/features/Items/Card/Card';
-import Header from '@/commons/components/widgets/Header/Header';
-import SearchToolbar from '@/commons/components/widgets/SearchToolbar/SearchToolbar';
-import * as styles from '@/commons/styles/rootStyles.css';
-
 export default function RootPage() {
-  return (
-    <div className={styles.wrapper}>
-      <Header />
-      <SearchToolbar />
-      <Card />
-      <RemoteControl />
-    </div>
-  );
+  return <div>UI 리뉴얼</div>;
 }
--- src/commons/components/atoms/boxes/tagBox/tagBox.tsx
+++
@@ -1,5 +0,0 @@
-import * as styles from './tagBox.css';
-
-export function TagBox({ tagName }: { tagName: string }) {
-  return <div className={styles.tagBox}>{tagName}</div>;
-}
